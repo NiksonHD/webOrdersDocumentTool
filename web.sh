3bb05f7ed465442469f27843bb162df5dccfb5bf
Nikolaj Dimitrov <niksondimitrov@gmail.com>
2021-05-18 13:59:45 +0300

    Clear some repeatments.

--- web.sh
+++ web.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+dir='downloads/web-orders/';
 IFS= read -d '' -n 1 orderString 
 while IFS= read -d '' -n 1 -t 1 c
 do
@@ -8,14 +9,14 @@ done
 echo '';
 
 read orderNum;
-echo '<head>' > downloads/web-orders/$orderNum.html; 
-echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">' >> downloads/web-orders/$orderNum.html;
-echo "<title>$orderNum</title>" >> downloads/web-orders/$orderNum.html;
-echo '<head>' >> downloads/web-orders/$orderNum.html;
-echo "<div> Web Order #$orderNum</div>" >> downloads/web-orders/$orderNum.html;
-echo '<ol>' >> downloads/web-orders/$orderNum.html;
+echo '<head>' > $dir$orderNum.html; 
+echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">' >> $dir$orderNum.html;
+echo "<title>$orderNum</title>" >> $dir$orderNum.html;
+echo '<head>' >> $dir$orderNum.html;
+echo "<div> Web Order #$orderNum</div>" >> $dir$orderNum.html;
+echo '<ol>' >> $dir$orderNum.html;
 for sapNum in ${orderString[@]};do
  echo  "<li><a target='_blank' href='https://praktiker.bg/bg/bg/Stolove/TRAPEZEN-STOL-LILOVI-KROS-2-VENGE/p/$sapNum'>$sapNum</a></li><br>" >> downloads/web-orders/$orderNum.html;
 
 done
-echo '</ol>' >> downloads/web-orders/$orderNum.html;
+echo '</ol>' >> $dir$orderNum.html;
