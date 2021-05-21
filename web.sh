bb590173eb07d3651e0cd4aca1cee5dd263c77ed
Nikolaj Dimitrov <niksondimitrov@gmail.com>
2021-05-21 13:24:05 +0300

    Clear some tipos.

--- web.sh
+++ web.sh
@@ -12,11 +12,12 @@ read orderNum;
 echo '<head>' > $dir$orderNum.html; 
 echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">' >> $dir$orderNum.html;
 echo "<title>$orderNum</title>" >> $dir$orderNum.html;
-echo '<head>' >> $dir$orderNum.html;
+echo '</head>' >> $dir$orderNum.html;
 echo "<div> Web Order #$orderNum</div>" >> $dir$orderNum.html;
 echo '<ol>' >> $dir$orderNum.html;
 for sapNum in ${orderString[@]};do
- echo  "<li><a target='_blank' href='https://praktiker.bg/bg/bg/Stolove/TRAPEZEN-STOL-LILOVI-KROS-2-VENGE/p/$sapNum'>$sapNum</a></li><br>" >> downloads/web-orders/$orderNum.html;
+ echo  "<li><a target='_blank' href='https://praktiker.bg/bg/bg/Stolove/TRAPEZEN-STOL-LILOVI-KROS-2-VENGE/p/$sapNum'>$sapNum</a></li><br>" >> $dir$orderNum.html;
 
 done
 echo '</ol>' >> $dir$orderNum.html;
+
